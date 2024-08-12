393cc3e4a7e6ed1f2c0ee51a42134f21bae54de8
blackPeppper <spices.digital@gmail.com>
2024-08-12 19:46:14 +0300

    perf: gziping and min

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import { minify } from '@zokki/astro-minify';
+import compress from 'astro-compressor';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({	integrations: [minify({logAllFiles: false}),compress({gzip: true, brotli: false})] });
